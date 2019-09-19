2f6cecb10c86c57af9c207caa264d3a7cbcc176c
Mauro Silberberg <maurosilber@gmail.com>
2019-09-19 12:17:54 -0300

    Fix setup.py
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='anisotropy',
-    version='0.0.0',
+    version='0.0.1',
+    packages=['anisotropy'],
+    url='https://github.com/maurosilber/anisotropy',
     license='MIT',
+    author='Mauro Silberberg',
+    author_email='maurosilber@gmail.com',
+    description='Fluorescence anisotropy microscopy analysis.',
+    install_requires=['numpy', 'pywt']
 )
